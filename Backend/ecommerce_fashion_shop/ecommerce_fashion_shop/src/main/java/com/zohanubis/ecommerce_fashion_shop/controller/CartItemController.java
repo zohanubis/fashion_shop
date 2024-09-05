b05f21ca29358a3b5d99ef91ad779aa0d10b3851
Zohanubis <zohanubis2509@gmail.com>
2024-09-05 08:30:03 +0700

    coding CartitemController

--- Backend/ecommerce_fashion_shop/ecommerce_fashion_shop/src/main/java/com/zohanubis/ecommerce_fashion_shop/controller/CartItemController.java
+++ Backend/ecommerce_fashion_shop/ecommerce_fashion_shop/src/main/java/com/zohanubis/ecommerce_fashion_shop/controller/CartItemController.java
@@ -1,14 +1,16 @@
 package com.zohanubis.ecommerce_fashion_shop.controller;
 
+import com.zohanubis.ecommerce_fashion_shop.exception.UserException;
 import com.zohanubis.ecommerce_fashion_shop.model.Cart;
 import com.zohanubis.ecommerce_fashion_shop.model.CartItem;
+import com.zohanubis.ecommerce_fashion_shop.model.User;
 import com.zohanubis.ecommerce_fashion_shop.service.CartItemService;
 import com.zohanubis.ecommerce_fashion_shop.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -19,5 +21,15 @@ public class CartItemController {
     ;
     @Autowired
     private UserService userService;
+
+
+    @GetMapping("/cardId")
+    @Operation(description = "Find cart item by cart Id")
+    public ResponseEntity<CartItem> findUserCartItem(@RequestHeader ("Authorization")String jwt) throws UserException {
+        User user = userService.findUserProfileByJwt(jwt);
+        CartItem cartItem = new CartItem();
+
+        return new ResponseEntity<>(cartItem, HttpStatus.OK);
+    }
 
 }
